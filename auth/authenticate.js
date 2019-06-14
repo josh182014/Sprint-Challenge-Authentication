17efd1b24395f9b12c731be3b041efa35a70caa3
josh182014 <josh182014@gmail.com>
2019-06-14 10:52:25 -0600

    use env variable for secret key

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -1,7 +1,5 @@
 const jwt = require('jsonwebtoken');
 
-const secrets = require('../config/secrets')
-
 const jwtKey =
   process.env.JWT_SECRET ||
   'add a .env file to root of project with the JWT_SECRET variable';
