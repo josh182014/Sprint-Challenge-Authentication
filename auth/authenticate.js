50332bf9413232d1974f90366d99c45c1c02c25a
josh182014 <josh182014@gmail.com>
2019-06-14 10:29:14 -0600

    added secret to .env file

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken');
 
+const secrets = require('../config/secrets')
+
 const jwtKey =
   process.env.JWT_SECRET ||
   'add a .env file to root of project with the JWT_SECRET variable';
