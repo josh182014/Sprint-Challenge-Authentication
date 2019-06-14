86887bd9e4ec72410e70e5c32561096dfbdbfb87
josh182014 <josh182014@gmail.com>
2019-06-14 10:03:51 -0600

    added tokens

--- config/routes.js
+++ config/routes.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs')
 
 const { authenticate } = require('../auth/authenticate');
 const Users = require('../users/users-model')
+const generateToken = require('./generateToken')
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -19,9 +20,9 @@ function register(req, res) {
   user.password = hash;
 
   Users.add(req.body)
-  .then(response => {
-    console.log(response)
-    res.status(200).json(response)
+  .then(user => {
+    const token = generateToken(user)
+    res.status(200).json({user: user, token})
   })
   .catch(error => {
     console.log(error)
@@ -36,7 +37,8 @@ function login(req, res) {
   Users.findByUsername(req.body.username)
   .then(user => {
     if (user && bcrypt.compareSync(password, user.password)) {
-      res.status(200).json(user)
+      const token = generateToken(user)
+      res.status(200).json({user: user, token})
     }
     else {
       res.status(401).json('Please enter valid credentials')
